a7e7389873386eb7be8b11c66dc0a08da772a75f
RNazarian1 <nazarianreza@gmail.com>
2021-10-19 02:26:27 +0000

    add user profile

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def register():
         # put the new user in the 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -51,14 +52,15 @@ def register():
 def login():
     if request.method == "POST":
         # check if username exists in DB
-        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
         if existing_user:
             # ensure hashed password matches input
             if check_password_hash(
                existing_user["password"], request.form.get("password")):
                    session["user"] = request.form.get("username").lower()
                    flash("Welcome, {}",format(request.form.get("username")))
-                   
+                   return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password
                 flash("Incorrect username and/or password")
@@ -71,9 +73,18 @@ def login():
 
     return render_template("login.html")
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username rom DB
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
-
